2880c7d3f37bea2c4b47533f3434e87da61cb49b
Koki Takahashi <hakatasiloving@gmail.com>
2022-10-01 18:59:29 +0900

    Implement loading poses

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import os
 import sys
 import bpy
 from mmd_tools.core.pmx.importer import PMXImporter
+from mmd_tools.core.vpd.importer import VPDImporter
+from mmd_tools.utils import makePmxBoneMap
 
 
 def example_function(model_path):
@@ -15,6 +17,11 @@ def example_function(model_path):
         types=set(['MESH', 'ARMATURE', 'PHYSICS', 'MORPHS', 'DISPLAY']),
         clean_model=True,
     )
+
+    model_object = bpy.context.active_object
+    for object in bpy.context.scene.collection.children.get('Collection').objects:
+        if object.type == 'ARMATURE':
+            model_object = object
 
     for material in bpy.data.materials:
         if '白目' in material.name:
@@ -46,6 +53,12 @@ def example_function(model_path):
                 continue
 
             diffuse_input.default_value = (1.0, 0.0, 0.0, 1.0)
+
+    importer = VPDImporter(
+        filepath=os.path.join(os.getcwd(), 'poses/stand1.vpd'),
+        bone_mapper=makePmxBoneMap,
+    )
+    importer.assign(model_object)
 
     bpy.ops.wm.save_as_mainfile(filepath='project_dump.blend')
 
